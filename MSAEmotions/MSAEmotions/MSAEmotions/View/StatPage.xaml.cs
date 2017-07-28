17340712f85009ead2a30abb8c998808f74a03d1
Aman Dwivedi <Sixthshadow42@users.noreply.github.com>
2017-07-28 14:46:13 +1200

    Added a statistics page

---
+++ MSAEmotions/MSAEmotions/MSAEmotions/View/StatPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MSAEmotions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatPage : ContentPage
+    {
+        public StatPage()
+        {
+            InitializeComponent();
+        }
+        async void RefreshStats(object sender, EventArgs e)
+        {
+            loading.IsRunning = true;
+            List<EmotionTable> emotionInformation = await EasyTablesBackend.EasyTablesBackendInstance.GetEmotion();
+            EmotionCount.Text = Convert.ToString(emotionInformation.Count);
+            CommonMood.Text = Convert.ToString(emotionInformation.GroupBy(s => s.Emotion).OrderByDescending(s => s.Count()).First().Key);
+            loading.IsRunning = false;
+        }
+    }
+}
